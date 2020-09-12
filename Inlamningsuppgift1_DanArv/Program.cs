e5757310d134272972ecc7a556e306ea2b6e05e9
DubbelD90 <dclock90@gmail.com>
2020-09-12 18:25:36 +0200

    Made changes and added random number generator

---
+++ Inlamningsuppgift1_DanArv/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace Inlamningsuppgift1_DanArv
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .Enrich.FromLogContext()
+            .WriteTo.File(@"C:\Users\dcloc\Documents\WService Documentation\Logfile.txt")
+            .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting worker service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, $"Worker service terminated unexpectedly, Error: {ex.Message}");
+                return;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .UseWindowsService()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ Inlamningsuppgift1_DanArv/Worker.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Inlamningsuppgift1_DanArv
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        
+        private readonly Random _rnd = new Random();
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("The service has started.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("The service has stoped.");
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                int Temp = _rnd.Next(-15, 45);
+                try
+                {
+
+                    if (Temp < 5)
+                    {
+                        _logger.LogInformation($"It's cold outside, dress warm. Temperature is: {Temp} degrees Celsius");
+                    }
+                    else if (Temp > 25)
+                    {
+                        _logger.LogInformation($"It's hot outside. The temperature is: {Temp} degrees Celsius");
+                    }
+                    else
+                    {
+                        _logger.LogInformation($"The current temperature is: {Temp} degrees Celsius");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    _logger.LogInformation($"Failed. Could not log temperature - {ex.Message}");
+                }
+
+                await Task.Delay(5*1000, stoppingToken);
+            }
+        }
+    }
+}
